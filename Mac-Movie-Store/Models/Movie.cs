a9607c856110609763d76317e8f5a207900d045c
Dawit <dawitde24@hotmail.com>
2019-08-13 17:40:16 -0700

    model movie

---
+++ Mac-Movie-Store/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mac_Movie_Store.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime ReleasedTime { get; set; }
+
+        public string Genre{ get; set; }
+        public int NumberOfCopy { get; set; }
+    }
+}
